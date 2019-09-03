4133fe1afe71ce156429771d6fce7132831d2aec
Matt <matt@skatanicstudios.co.uk>
2019-09-03 22:35:39 +0100

    Initial commit

---
+++ Editor/EditorCameraController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.InputSystem;
+
+[InitializeOnLoad()]
+public class EditorCameraController
+{
+    static EditorCameraController()
+    {
+        EditorApplication.update += Update;
+    }
+
+    public static Vector3 position;
+    public static Quaternion rotation;
+
+    static void Update()
+    {
+        /*
+
+
+        Debug.Log("Updating");
+        */
+        var gamepad = Gamepad.current;
+        if (gamepad == null)
+        {
+            //Debug.Log("NO GAMEPAD CONNECTED");
+            return; // No gamepad connected.
+        }
+
+        Vector2 move = gamepad.leftStick.ReadValue();
+        Vector2 rotate = gamepad.rightStick.ReadValue();
+        bool up = gamepad.rightShoulder.isPressed;
+        bool down = gamepad.leftShoulder.isPressed;
+        bool rightStick = gamepad.rightStickButton.isPressed;
+
+
+        GameObject _cameraTarget = new GameObject();
+        _cameraTarget.transform.position = position;
+        _cameraTarget.transform.rotation = rotation;
+
+
+        _cameraTarget.transform.position += _cameraTarget.transform.forward * move.y;
+        _cameraTarget.transform.position += _cameraTarget.transform.right * move.x;
+        _cameraTarget.transform.position += up ? _cameraTarget.transform.up * .1f : _cameraTarget.transform.up * 0f;
+        _cameraTarget.transform.position -= down ? _cameraTarget.transform.up * .1f : _cameraTarget.transform.up * 0f;
+
+        _cameraTarget.transform.Rotate(Vector3.up, rotate.x * 5);
+        _cameraTarget.transform.Rotate(Vector3.right, -rotate.y * 5);
+        //Levelling out the z angle makes for a smoother look rotation.
+        _cameraTarget.transform.rotation = Quaternion.Euler(_cameraTarget.transform.eulerAngles.x, _cameraTarget.transform.eulerAngles.y, 0);
+
+        position = _cameraTarget.transform.position;
+        rotation = _cameraTarget.transform.rotation;
+        
+        SceneView view = SceneView.lastActiveSceneView;
+        view.pivot = position;
+        view.rotation = rotation;
+        view.size = 0;
+        Debug.Log(view.cameraDistance);
+
+        GameObject.DestroyImmediate(_cameraTarget);
+
+        //TODO: Add support for editing settings.
+    }
+}
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Project Title
+```
+This has been tested on Unity 2019.1.14f and requires the Input System package (v0.9.5 or later) to run.
+
+```
+This script allows you to navigate through the scene view and control the camera using a gamepad.
+
+## Getting Started
+
+* 1) Pull or download the repo. 
+* 2) Copy Editor folder into your project.
+* 3) Plug in gamepad and naviagte through scene.
+
+![Preview](https://raw.githubusercontent.com/)
+
+## License
+
+This is licensed under the MIT License.
+
+##Twitter
+Feel free to [Tweet me](http://twitter.com/mattgambell) if you're using it!
